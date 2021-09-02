b73e82320c591f457bd82067f7e2faff81168016
Hailee Vandiver <84043760+HaileeVandiver@users.noreply.github.com>
2021-09-02 15:49:17 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,25 +1,31 @@
 import java.util.ArrayList;
+
 public class Player {
     ArrayList<Card> cards;
 
+    //setting the size to 52
     public Player(){
-        cards = new ArrayList<Card>(52); //setting the size to 52
+        cards = new ArrayList<Card>(52);
     }
 
-    public void getCard(Card aCard){ //get card from deck to player pile
+    //get card from deck to player pile
+    public void getCard(Card aCard){
         cards.add(aCard);
     }
-//pull a card from the zero position of the arraylist of player
-    public Card playCard(){ 
+    //pull a card from the zero position of the arraylist of player
+    public Card playCard(){
         return cards.remove(0);
     }
 
-    public Card seeCard(int position){// shows card at any given position in deck (used for debugging)
+    // shows card at any given position in deck (used for debugging)
+    public Card seeCard(int position){
         return cards.get(position);
     }
 
-    public int cardCount(){//returns the number of cards that are in each players arraylist
+    //returns the number of cards that are in each players arraylist
+    public int cardCount(){
         return cards.size();
     }
 }
+
 
