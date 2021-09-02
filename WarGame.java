e7e28db26907238134eb879045ab37d01ab618c6
Hailee Vandiver <84043760+HaileeVandiver@users.noreply.github.com>
2021-09-02 15:56:46 -0700

    Update WarGame.java

--- WarGame.java
+++ WarGame.java
@@ -4,20 +4,30 @@ public class WarGame {
 
 
     public static void main(String[] args) {
-        //each player
+        /**
+        *each player
+        */
         Player p1, p2;
 
-        //the cards being played
+        /**
+        *the cards being played
+        */
         Card p1Card, p2Card;
 
-        // deck object
+        /**
+        *deck object
+        */
         Deck theDeck;
 
-        // each player's hand. list to hold cards
+        /**
+        *each player's hand. list to hold cards
+        */
         ArrayList<Card> p1Pile, p2Pile;
 
 
-        // to see if "war" has already happened
+        /**
+        *to see if "war" has already happened
+        */
         boolean justWarred;
 
         justWarred = false;
@@ -29,19 +39,29 @@ public class WarGame {
 
         theDeck.shuffle();
 
-        // distribute 26 cards to each players pile
+        /**
+        *distribute 26 cards to each players pile
+        */
         for (int i = 0; i < 26; i++) {
-            //tell p1 to get a card from the deck
+            /**
+            *tell p1 to get a card from the deck
+            */
             p1.getCard(theDeck.deal());
-            // tell p2 to get a card from the deck
+            /**
+            *tell p2 to get a card from the deck
+            */
             p2.getCard(theDeck.deal());
         }
 
 
-        // string to hold who won each round
+        /**
+        *string to hold who won each round
+        */
         String winner = "";
 
-        //enter game loop
+        /**
+        *enter game loop
+        */
         while (true) {
                 for (int i=0; i< p1.cardCount(); i++)
                     System.out.print("[]");
@@ -51,13 +71,19 @@ public class WarGame {
                 System.out.println();
                 System.out.println("Player 1 stack="+p1.cardCount()+" "+"Player 2 stack="+p2.cardCount());
 
-            // no cards on the "table", each players puts down a card
+            /**
+            *no cards on the "table", each players puts down a card
+            */
             if (!justWarred) {
-                //if the player has more than 0 cards
+                /**
+                *if the player has more than 0 cards
+                */
                 if (p1.cardCount() > 0)
                     p1Pile.add(p1.playCard());
                 else {
-                    //player 1 has 0 cards
+                    /**
+                    *player 1 has 0 cards
+                    */
                     winner = "p2";
                     break;
 
@@ -66,15 +92,23 @@ public class WarGame {
                     p2Pile.add(p2.playCard());
 
                 else {
-                    // player 2 has 0 cards
+                    /**
+                    *player 2 has 0 cards
+                    */
                     winner = "p1";
                     break;
                 }
             }
             justWarred = false;
-            //get card from top of pile
+            /**
+            *get card from top of pile
+            */
             p1Card = p1Pile.get(p1Pile.size() - 1);
             p2Card = p2Pile.get(p2Pile.size() - 1);
+            
+            /**
+            *show cards
+            */
             if(p1Card!=null) System.out.println("Player 1 card="+p1Card.toString());
             if(p2Card!=null) System.out.println("Player 2 card="+p2Card.toString());
             for(int i=0; i<p1.cardCount();i++)
@@ -88,7 +122,9 @@ public class WarGame {
 
             }
 
-            // comparisons to determine who has the bigger card
+            /**
+            *comparisons to determine who has the bigger card
+            */
             if (p1Card.getPointValue() > p2Card.getPointValue())
                 winner = "p1";
             else if (p2Card.getPointValue() > p1Card.getPointValue())
@@ -96,18 +132,26 @@ public class WarGame {
             else
                 winner = "none";
 
-            //put all cards into p1 pile
+            /**
+            *put all cards into p1 pile
+            */
             if (winner.equals("p1")) {
-                //make sure p1 pile has at least 1 card
+                /**
+                *make sure p1 pile has at least 1 card
+                */
                 while (p1Pile.size() > 0)
                     p1.getCard(p1Pile.remove(0));
                 while (p2Pile.size() > 0)
                     p1.getCard(p2Pile.remove(0));
             }
 
-            //put all cards into p2 pile
+            /**
+            *put all cards into p2 pile
+            */
             if (winner.equals("p2")) {
-                //make sure p2 pile has at least 1 card
+                /**
+                *make sure p2 pile has at least 1 card
+                */
                 while (p2Pile.size() > 0)
                     p2.getCard(p2Pile.remove(0));
                 while (p1Pile.size() > 0)
@@ -117,7 +161,8 @@ public class WarGame {
             if (winner.equals("none")) {
                 justWarred = true;
                 int count = 0;
-                /*pull up to 4 cards from each player, if the player has < 4 the last card in the stack is
+                /**
+                 *pull up to 4 cards from each player, if the player has < 4 the last card in the stack is
                  *used to determine winner
                  */
                 while (p1.cardCount() > 0 && count < 4) {
