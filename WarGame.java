06fabd9197a4c4493704b188ac8a683ca7d715a6
Hailee Vandiver <84043760+HaileeVandiver@users.noreply.github.com>
2021-09-01 20:32:52 -0700

    Update WarGame.java

--- WarGame.java
+++ WarGame.java
@@ -1,47 +1,36 @@
 
-/**
- * Write a description of WarGame here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class WarGame {
-   
-    Player p1, p2;// each player
-    Card p1Card, p2Card;//the cards being played
-    Deck theDeck;// deck object 
-    ArrayList<Card> p1Pile, p2Pile;// each player's hand. list to hold cards
-    boolean justWarred; // to see if "war" has already happened
+import java.util.ArrayList;
 
-    public WarGame(){//constructor
+public class WarGame {
+
+    public static void main(String[] args){
+        Player p1, p2;// each player
+        Card p1Card, p2Card;//the cards being played
+        Deck theDeck;// deck object 
+        ArrayList<Card> p1Pile, p2Pile;// each player's hand. list to hold cards
+        boolean justWarred; // to see if "war" has already happened
         justWarred = false;
         p1Pile = new ArrayList<Card>();
         p2Pile = new ArrayList<Card>();
         p1 = new Player();
         p2 = new Player();
-        String[] suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
-        String [] ranks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", 
-            "Jack", "Queen", "King"};
-        int[] values = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-        theDeck = new Deck(ranks,suits,values);
+        theDeck = new Deck();
         theDeck.shuffle();
-        for (int i=0; i<26; i++) {// distribute 26 cards to each players hand
+        
+        for (int i=0; i<26; i++) {// distribute 26 cards to each players pile
             p1.getCard(theDeck.deal());//tell p1 to get a card from the deck
             p2.getCard(theDeck.deal());// tell p2 to get a card from the deck
         }
-        beginGame();
-    }
-   
-    private void beginGame(){
+
+
         String winner = "";// string to hold who won each round
         while(true){//enter game loop
-            showCards();//method for printing
             if(!justWarred) {// no cards on the "table", each players puts down a card
                 if (p1.cardCount() > 0)//if the player has more than 0 cards
                     p1Pile.add(p1.playCard());
                 else {
                     winner = "p2";//player 1 has 0 cards
-                    break;//just here to look pretty
+                    break;
                 }
                 if (p2.cardCount() > 0)
                     p2Pile.add(p2.playCard());
@@ -55,9 +44,9 @@ public class WarGame {
             p2Card = p2Pile.get(p2Pile.size()-1);
 
             // comparisons to determine who has the bigger card
-            if (p1Card.compareTo(p2Card)>0)
+            if (p1Card.getPointValue() > p2Card.getPointValue())
                 winner = "p1";
-            else if (p1Card.compareTo(p2Card)<0)
+            else if (p2Card.getPointValue() > p1Card.getPointValue())
                 winner = "p2";
             else
                 winner = "none";
@@ -69,9 +58,9 @@ public class WarGame {
                     p1.getCard(p2Pile.remove(0));
             }
             if (winner.equals("p2")) {//put all cards into p2 pile
-                while (p2Pile.size() > 0)//make sure p1 pile has at least 1 card
-                    p2.getCard(p1Pile.remove(0));
-                while (p2Pile.size() > 0)
+                while (p2Pile.size() > 0)//make sure p2 pile has at least 1 card
+                    p2.getCard(p2Pile.remove(0));
+                while (p1Pile.size() > 0)
                     p2.getCard(p1Pile.remove(0));
             }
 
