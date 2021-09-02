2f6cc2af4b7e84945d66404abf548cc496227f0d
Hailee Vandiver <84043760+HaileeVandiver@users.noreply.github.com>
2021-09-02 15:58:11 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,20 +1,32 @@
 public class Card {
 
-    //String that holds suit of card
+    /**
+    *String that holds suit of card
+    */
     private String suit;
 
-    //String that holds rank of card(10,Jack, Queen, etc.)
+    /**
+    *String that holds rank of card(10,Jack, Queen, etc.)
+    */
     private String rank;
 
-    //Integer that holds the point value (10,11,12,etc.)
+    /**
+    *Integer that holds the point value (10,11,12,etc.)
+    */
     private int pointValue;
 
     public Card(String rank, String suit, int pointValue){
-        // initialize rank of card
+        /**
+        *initialize rank of card
+        */
         this.rank = rank;
-        //initialize suit of card
+        /**
+        *initialize suit of card
+        */
         this.suit = suit;
-        //initialize the value of card
+        /**
+        *initialize the value of card
+        */
         this.pointValue = pointValue;
     }
     public String getSuit(){
