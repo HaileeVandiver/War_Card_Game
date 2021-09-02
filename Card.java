1a01660df48c4e75f1bdc85b8fe1a78b41b0f185
Hailee Vandiver <84043760+HaileeVandiver@users.noreply.github.com>
2021-09-01 20:31:43 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,10 +1,4 @@
 
-/**
- * Write a description of Card here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Card {
    
     private String suit; //String that holds suit of card
@@ -13,29 +7,26 @@ public class Card {
     
     private int pointValue; //Integer that holds the point value (10,11,12,etc.)
     
-    public Card(String cardRank, String cardSuit, int cardPointValue){
-        rank = cardRank;// initialize rank of card
-        suit = cardSuit; //initialize suit of card
-        pointValue = cardPointValue; //initialize the value of card
+    public Card(String rank, String suit, int pointValue){
+        this.rank = rank;// initialize rank of card
+        this.suit = suit; //initialize suit of card
+        this.pointValue = pointValue; //initialize the value of card
     }
-    public String suit(){
+    public String getSuit(){
         return suit;
     }
-    public String rank() {
+    public String getRank() {
         return rank;
     }
-    public int pointValue(){
+    public int getPointValue(){
         return pointValue; 
     }
-    public boolean matches(Card otherCard){
-        return (this.suit.equals(otherCard.suit()) 
-            && this.rank.equals(otherCard.rank())
-            && this.pointValue==otherCard.pointValue());
-    }
+   
     @Override 
     public String toString(){
         return suit  + rank  + pointValue; 
     }
 }
+   
     
 
